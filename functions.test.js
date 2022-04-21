72f45e6ec21b0386678b55087c3c8f0a46ab1d4f
ciamac-da <ciamac.davoudi@gmail.com>
2022-04-21 10:52:26 +0200

    Verified by Marti4n v1.0.3

--- functions.test.js
+++ functions.test.js
@@ -30,3 +30,43 @@ test("User should be Ciamac Da object", () => {
         lastName: "Da"
     })
 })
+
+// tobeLessThan
+test("Should be under 1600", () => {
+    const load1 = 800
+    const load2 = 700
+    expect(load1 + load2).toBeLessThan(1600)
+})
+
+// toBeLessThanOrEqual
+test("Should be under 1600", () => {
+    const load1 = 800
+    const load2 = 800
+    expect(load1 + load2).toBeLessThanOrEqual(1600)
+})
+
+// toBeGreaterThan
+test("Should be under 1600", () => {
+    const load1 = 800
+    const load2 = 900
+    expect(load1 + load2).toBeGreaterThan(1600)
+})
+
+
+// toBeGreaterThanOrEqual
+test("Should be under 1600", () => {
+    const load1 = 800
+    const load2 = 800
+    expect(load1 + load2).toBeGreaterThanOrEqual(1600)
+})
+
+// Regex
+test("There is no I in team", () => {
+    expect("team").not.toMatch(/I/i)
+})
+
+// Arrays
+test("Admin should be in usernames", () => {
+    usernames = ["admin", "user", "root"]
+    expect(usernames).toContain("root")
+})
