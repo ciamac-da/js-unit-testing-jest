79993cc58caaf5fb710e978e7692e41b5f32e973
ciamac-da <ciamac.davoudi@gmail.com>
2022-04-21 08:32:51 +0200

    Verified by Marti4n v1.0.2

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,32 @@
+// Importing the functions file
 const functions = require("./functions")
 
+// toBe
 test("Adds 2 + 2 to equal 4", () => {
     expect(functions.add(2, 2)).toBe(4)
 })
 
+// not.toBe
 test("Adds 2 + 2 to NOT equal 5", () => {
     expect(functions.add(2, 2)).not.toBe(5)
+})
+
+// toBeNull
+test("Should be null", () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+// toBeFalsy
+test("Should be falsy", () => {
+    // Here you can also use 0 or undefined instead of false
+    // because all of them are falsy
+    expect(functions.checkValue(false)).toBeFalsy()
+})
+
+// toEqual
+test("User should be Ciamac Da object", () => {
+    expect(functions.createUser()).toEqual({
+        firstName: "Ciamac",
+        lastName: "Da"
+    })
 })
